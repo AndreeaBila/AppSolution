e6db0c1924bf5f667c685ff9485da0d7f016bede
SpecDI <vlad@herghelegiu.com>
2017-07-09 02:10:49 +0300

    asd

--- js/home.js
+++ js/home.js
@@ -1,3 +1,4 @@
+//object needed to index the site views
 //function needed to defer the javascript 
 $(function() {
     //get the dimension of the screen and set mobile/pc view
@@ -12,7 +13,6 @@ $(function() {
 
 //this function is needed to adjust the webpage to the mobile version
 function loadMobile() {
-    alert('Im in mobile!');
     //on page load hide everything but presentation
     $('#portofolioPage').hide();
     $('#aboutPage').hide();
