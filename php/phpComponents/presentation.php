497d43db84e4a7cfaa5c508c7c88d068cee8c687
AndreeaBila <andreeabila@yahoo.com>
2017-07-10 21:28:18 +0300

    cctule

--- php/phpComponents/presentation.php
+++ php/phpComponents/presentation.php
@@ -6,19 +6,49 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque asperiores, quibusdam excepturi suscipit perferendis ipsa, hic amet ullam sint modi unde praesentium eaque consequatur dolorum nisi recusandae aliquid voluptas quia.</p>
   </div>
 
-  <div class="jumbotron offerJumbotron text-center">
-    <h2>Our services</h2>
+  <div class="jumbotron offerJumbotron">
+    <h2 class="text-center">Our services</h2>
     <div>
-      <h3>Front End</h3>
-      <div class="col-xs-4">
-        Web Design
+    
+      <div class="col-xs-12 col-md-4">
+        <div class="offerComponent">
+          <img class=" pull-left" src="../img/design.png" alt="Image unavailable">
+          <p>Website Design</p>
+          <div class="clear"></div>
+        </div>
+        <div class="offerComponent">
+          <img class=" pull-left" src="../img/responsive.png" alt="Image unavailable">
+          <p>Responsive Layout</p>
+          <div class="clear"></div>
+        </div>
       </div>
-      <div class="col-xs-4">
-        Html
+       
+      <div class="col-xs-12 col-md-4">
+        <div class="offerComponent">
+          <img class=" pull-left" src="../img/design.png" alt="Image unavailable">
+          <p>Website Design</p>
+          <div class="clear"></div>
+        </div>
+        <div class="offerComponent">
+          <img class=" pull-left" src="../img/responsive.png" alt="Image unavailable">
+          <p>Responsive Layout</p>
+          <div class="clear"></div>
+        </div>
       </div>
-      <div class="col-xs-4">
-        Css & Sass
+
+      <div class="col-xs-12 col-md-4">
+        <div class="offerComponent">
+          <img class=" pull-left" src="../img/design.png" alt="Image unavailable">
+          <p>Website Design</p>
+          <div class="clear"></div>
+        </div>
+        <div class="offerComponent">
+          <img class=" pull-left" src="../img/responsive.png" alt="Image unavailable">
+          <p>Responsive Layout</p>
+          <div class="clear"></div>
+        </div>
       </div>
+
     </div>
     
   </div>
