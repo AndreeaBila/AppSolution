63b0bb38eea66cf82669fc2f580ea92704fbaa3d
SpecDI <vlad@herghelegiu.com>
2017-08-03 20:27:21 +0300

    map changed

--- php/phpComponents/contact.php
+++ php/phpComponents/contact.php
@@ -1,6 +1,6 @@
 <!--This is the Contact part of the page, where we give our contact details-->
 <section id="contactPage">
   <div class="contactJumbotron">
-    <iframe src="//mapkit.io/embed/7yQlen" frameborder="0" id="map"></iframe> 
+    <iframe src="//mapkit.io/embed/7yQlen" id="map"></iframe> 
   </div>  
 </section>
