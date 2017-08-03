5016a8ad17f7b1ad570e3843ff4d0c7851efb90f
SpecDI <vlad@herghelegiu.com>
2017-08-03 18:20:56 +0300

    map updated

--- php/phpComponents/contact.php
+++ php/phpComponents/contact.php
@@ -1,6 +1,5 @@
 <!--This is the Contact part of the page, where we give our contact details-->
 <section id="contactPage">
   <div class="contactJumbotron">
-     <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1K1ZlY84jta9kWOXtkCb-18xKESA" id="map"></iframe> 
-  </div>  
+<iframe src="//mapkit.io/embed/2faMwh" frameborder="0" id="map"></iframe>  </div>  
 </section>
